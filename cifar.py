bug fixed for masking

cifar.py
@@ -20,6 +20,7 @@ from utils.indicators import l2_reg_ortho, get_ntk, ntk_differentiable
 from utils.logger import prepare_logger, prepare_seed
 
 import models
+import warnings
 from pdb import set_trace as bp
 
 visible_gpus_str = ','.join(str(i) for i in args.gpus)
@@ -58,6 +59,7 @@ def train(model, optimizer, trainLoader, args, epoch, logger, model_dense=None):
     pbar = tqdm(trainLoader, position=0, leave=True)
     # for batch, (inputs, targets) in enumerate(trainLoader):
     for batch, (inputs, targets) in enumerate(pbar):
+        if batch > 10: break # TODO force end epoch to save checkpoint
         loss = 0; output = None
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
@@ -210,9 +212,9 @@ def main():
     best_acc = 0.0
 
     prepare_seed(args.rand_seed)
-    model, pr_cfg = get_model(args, logger, pretrained=args.pretrained_model!='')
+    model, pr_cfg = get_model(args, logger)
     if args.teacher:
-        model_dense, _ = get_model(args, logger, pretrained=args.pretrained_model!='', sparse=False)
+        model_dense, _ = get_model(args, logger, sparse=False)
     optimizer = get_optimizer(args, model)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, args.num_epochs)
 
@@ -266,14 +268,16 @@ def resume(args, model, optimizer):
         print(f"=> No checkpoint found at '{args.job_dir}' '/checkpoint/")
 
 
-def get_model(args, logger, pretrained=True, sparse=True):
+def get_model(args, logger, sparse=True):
     pr_cfg = []
 
     print("=> Creating model '{}'".format(args.arch))
     model = models.__dict__[args.arch]().to(device)
-    if pretrained:
+    try:
         ckpt = torch.load(args.pretrained_model, map_location=device)
         model.load_state_dict(ckpt['state_dict'], strict=False)
+    except Exception as e:
+        warnings.warn("Fail to load checkpoint: {}".format(e))
 
     # applying sparsity to the network
     pr_cfg = generate_pr_cfg(model)

utils/common.py
@@ -117,7 +117,7 @@ class checkpoint():
         record_config(args)
 
     def save_model(self, state, epoch, is_best):
-        save_path = f'{self.ckpt_dir}/model_last.pt'
+        save_path = f'{self.ckpt_dir}/epoch{epoch}.pt'
         torch.save(state, save_path)
         if is_best:
             shutil.copyfile(save_path, f'{self.ckpt_dir}/model_best.pt')

utils/conv_type.py
@@ -8,6 +8,7 @@ import math
 
 from utils.options import args as parser_args
 import numpy as np
+from pdb import set_trace as bp
 
 LearnedBatchNorm = nn.BatchNorm2d
 
@@ -21,15 +22,22 @@ DenseConv = nn.Conv2d
 class GetMask(autograd.Function):
     @staticmethod
     def forward(ctx, mask, prune_rate):
-        out = mask.clone()
+        # out = mask.clone()
         _, idx = mask.flatten().sort()
         j = int(prune_rate * mask.numel())
 
-        # flat_out and out access the same memory.
-        flat_out = out.flatten()
-        flat_out[idx[:j]] = 0
-        flat_out[idx[j:]] = 1
-        return out
+        # # flat_out and out access the same memory.
+        # flat_out = out.flatten()
+        # # flat_out[idx[:j]] = 0
+        # # flat_out[idx[j:]] = 1
+        # flat_out[idx[:j]] = (0 - flat_out[idx[:j]]).detach() + flat_out[idx[:j]]
+        # flat_out[idx[j:]] = (1 - flat_out[idx[j:]]).detach() + flat_out[idx[j:]]
+        # return out
+
+        mask_hard = torch.zeros_like(mask.view(-1))
+        mask_hard[idx[j:]] = 1
+        mask_hard = (mask_hard - mask.view(-1)).detach() + mask.view(-1)
+        return mask_hard.view(mask.shape)
 
     @staticmethod
     def backward(ctx, g):
@@ -48,7 +56,8 @@ class PretrainConv(nn.Conv2d):
         # sparseWeight = mask * self.weight
         x = F.conv2d(
             # x, sparseWeight, self.bias, self.stride, self.padding, self.dilation, self.groups
-            x, self.sparse_weight, self.bias, self.stride, self.padding, self.dilation, self.groups
+            # x, self.sparse_weight, self.bias, self.stride, self.padding, self.dilation, self.groups
+            x, self.weight * self.mask, self.bias, self.stride, self.padding, self.dilation, self.groups
         )
         return x
 